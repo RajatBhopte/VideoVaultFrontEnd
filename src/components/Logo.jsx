36c4bf2e15c3f4aa86f560936f2361fae808f29d
Rajat Bhopte <114064979+RajatBhopte@users.noreply.github.com>
2025-11-10 22:45:05 +0530

    Fix typo in logo text from 'VideoWalt' to 'VideoVault'

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -10,7 +10,7 @@ function Logo({ size = "30" }) {
                     size={size}
                     color="#A855F7"
                 />
-                <span className="font-bold text-white">VideoWalt</span>
+                <span className="font-bold text-white">VideoVault</span>
             </Link>
         </>
     );
